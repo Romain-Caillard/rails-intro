69ac5059b1c0fdf83c3885cd75e74dd0161227c0
romain caillard <mttdiamond@gmail.com>
2023-08-07 18:17:13 +0200

    step

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
 class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+    # logique qui permettra de construire la page
+  end
+
+  def contact
+    # logique qui permettra de construire la page
+  end
 end
